d9137ad71753a17843498a424cb3b26eeb757445
Munkin <Pigsizaz@hotmail.com>
2017-08-12 12:08:59 -0500

    Add files via upload

---
+++ parcial 1 finals/IDED_Scripting_Parcial1/Assets/Scripts/Catapult.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Catapult : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject projectileToFire;
+
+    [SerializeField]
+    private Transform projectileSpawnTranform;
+
+    public void Fire()
+    {
+       GameObject tempBullet = Instantiate(projectileToFire, projectileSpawnTranform.position, projectileSpawnTranform.rotation);
+       tempBullet.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * 5f);
+    }
+}
---
+++ parcial 1 finals/IDED_Scripting_Parcial1/Assets/Scripts/InputManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+	[SerializeField]
+	private GameController miGameController;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			miGameController.ActiveBase.AttackWithCatapult();
+		}
+
+		if(Input.GetKeyDown(KeyCode.S))
+		{
+			miGameController.ActiveBase.AttackWithRay();
+		}
+	
+	}
+}
